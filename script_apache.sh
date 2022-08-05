1e8457b83a49f4e420b223ddd8b67a539a35af71
Marcos Gomes <39837780+marquinhosvlka@users.noreply.github.com>
2022-08-05 16:27:54 -0300

    Update script_apache.sh

--- script_apache.sh
+++ script_apache.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "atualizando o server"
 sudo apt-get update
-sudo apt-get upgrade -y
+sudo apt-get upgrade
 
 echo "baixando as aplicações"
 
-sudo apt-get install apache2 -y
-sudo apt-get instal unzip -y
+sudo apt-get install apache2
+sudo apt-get instal unzip
 cd /tmp
-wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip &
 unzip main.zip
 cd linux-site-dio
 cp -R * /var/www/html/
