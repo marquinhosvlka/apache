3bf03499320d243b8e71970c79b6047ba2a406e0
Marcos Gomes <39837780+marquinhosvlka@users.noreply.github.com>
2022-08-05 15:52:32 -0300

    Add files via upload

---
+++ script_apache.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "atualizando o server"
+sudo apt-get update
+sudo apt-get upgrade -y
+
+echo "baixando as aplicações"
+
+sudo apt-get install apache2 -y
+sudo apt-get instal unzip -y
+cd /tmp
+wget htpps://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio
+cp -R * /var/www/html/
+
+
+
